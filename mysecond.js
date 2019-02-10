fca0cb7e66cf3399ebbd49347ed7b7e76725c3ac
anawad <anawad@umich.edu>
2019-02-09 20:34:09 -0500

    ugg

--- mysecond.js
+++ mysecond.js
@@ -1,7 +1,7 @@
 var express = require('express')
-var app = express()
+var app = express.Router()
 
 // respond with "hello world" when a GET request is made to the homepage
-app.post('/', function (req, res) {
+app.post('/blaise', function (req, res) {
   res.send('hello world')
 })
