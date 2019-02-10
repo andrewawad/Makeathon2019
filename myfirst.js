dbd19071f671850cea99f18a7323d099733e4654
anawad <anawad@umich.edu>
2019-02-09 20:45:29 -0500

    backtobasics

--- myfirst.js
+++ myfirst.js
@@ -1,18 +1,11 @@
 var express = require("express");
 var myParser = require("body-parser");
 var app = express.Router();
- 
-//app.use(myParser.urlencoded({extended : true}));
-//app.post("/saam", function(request, response) {
-//res.write(request.body); //This prints the JSON document received (if it is a JSON document)
-// });
+var p = require(./mysecond)
 var http = require('http');
-port = process.env.PORT || 80 ||8080
+port = process.env.PORT || 80 
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.write('Hello World!');  
-app.use(myParser.urlencoded({extended : true}));
-app.post("/", function(request, response) => {
-response.write(request.body); //This prints the JSON document received (if it is a JSON document:e)
 });
 }).listen(port);
