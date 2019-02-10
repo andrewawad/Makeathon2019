417317b0a0a38a2b97c9f7e9b433cd02c9c1056f
anawad <anawad@umich.edu>
2019-02-09 19:31:16 -0500

    write

--- myfirst.js
+++ myfirst.js
@@ -2,15 +2,17 @@ var express = require("express");
 var myParser = require("body-parser");
 var app = express();
  
+//app.use(myParser.urlencoded({extended : true}));
+//app.post("/sam", function(request, response) {
+//res.write(request.body); //This prints the JSON document received (if it is a JSON document)
+// });
+var http = require('http');
+port = process.env.PORT || 80
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.write('Hello World!');  
 app.use(myParser.urlencoded({extended : true}));
 app.post("/sam", function(request, response) {
 res.write(request.body); //This prints the JSON document received (if it is a JSON document)
  });
-
-var http = require('http');
-port = process.env.PORT || 80
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
 }).listen(port);
