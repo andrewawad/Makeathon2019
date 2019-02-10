72eb48a0f535f61518e5d6c447a29afc44fc3d97
anawad <anawad@umich.edu>
2019-02-09 20:49:00 -0500

    revert

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,8 @@
-var express = require("express");
-var myParser = require("body-parser");
-var app = express.Router();
-var p = require(./mysecond)
 var http = require('http');
-port = process.env.PORT || 80 
+port = process.env.PORT || 80
 http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.write('Hello World!');  
-});
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
 }).listen(port);
